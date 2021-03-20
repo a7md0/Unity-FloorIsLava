e1c98e9671d538a10515e7037509fda72032af97
a7md0 <a7m3d699@gmail.com>
2021-03-21 00:52:27 +0300

    Fix collect coins not working

---
+++ Assets/Scripts/CollectCoins.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoins : MonoBehaviour
+{
+    private int score = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin") {
+            score++;
+            print("Score " + score);
+            Destroy(other.gameObject);
+        }
+    }
+}
